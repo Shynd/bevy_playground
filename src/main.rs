a0b5e28cbcb3ff6e0deb2b5fc19b3a5045991aea
Shynd <shynd@protonmail.com>
2020-09-05 21:55:46 +0200

    added person system

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::build().add_system(hello_world.system()).run();
+    App::build()
+        .add_startup_system(add_people.system())
+        .add_system(hello_world.system())
+        .run();
 }
 
 fn hello_world() {
     println!("hello, world!")
+}
+
+// Person component
+struct Person;
+
+// Name component
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn((Person, Name("Amicus".to_string())))
+        .spawn((Person, Name("Cassius".to_string())))
+        .spawn((Person, Name("Alexios".to_string())));
 }
